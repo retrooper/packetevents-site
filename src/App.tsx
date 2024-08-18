edc218db2c6edf4d4fecb4424505585c84551cae
retrooper <retrooperdev@gmail.com>
2024-08-18 16:35:15 +0200

    added nice gradient transition to the site footer, removed unwanted features

--- src/App.tsx
+++ src/App.tsx
@@ -86,8 +86,8 @@ import {
 
 import { Input } from "@/components/ui/input";
 
-import { ToastAction } from "@/components/ui/toast";
-import { useToast } from "@/components/ui/use-toast";
+import { toast } from "sonner";
+import { Toaster } from "@/components/ui/sonner";
 
 const components: { title: string; href: string; description: string }[] = [
   {
@@ -129,15 +129,14 @@ const components: { title: string; href: string; description: string }[] = [
 
 function App() {
   const { setTheme } = useTheme();
-  const { toast } = useToast();
 
   return (
     <>
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        <div className="space-y-7">
-          <div className="absolute top-2 left-0 hover:drop-shadow-[0_35px_35px_rgba(100,120,255,0.8)]">
-            <div className="hover:animate-[wiggle_infinite_15s_linear]">
-              <img width="70" height="70" src={packetEventsLogo} alt="Logo" />
+        <div className="space-y-5">
+          <div className="absolute top-7 left-0 hover:drop-shadow-[0_35px_35px_rgba(100,120,255,1)]">
+            <div className="flex justify-center mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-200 select-none">
+              packetevents
             </div>
           </div>
           <div className="absolute top-0 right-4 hover:drop-shadow-[0_35px_35px_rgba(100,120,255,0.8)]">
@@ -189,13 +188,8 @@ function App() {
                           href="/"
                         >
                           <div className="mb-2 mt-4 text-lg font-medium">
-                            shadcn/ui
+                            packetevents
                           </div>
-                          <p className="text-sm leading-tight text-muted-foreground">
-                            Beautifully designed components that you can copy
-                            and paste into your apps. Accessible. Customizable.
-                            Open Source.
-                          </p>
                         </a>
                       </NavigationMenuLink>
                     </li>
@@ -208,29 +202,14 @@ function App() {
                     </ListItem>
                     <ListItem
                       href="/docs/primitives/typography"
-                      title="Typography"
+                      title="Advanced Examples"
                     >
                       Styles for headings, paragraphs, lists...etc
                     </ListItem>
                   </ul>
                 </NavigationMenuContent>
               </NavigationMenuItem>
-              <NavigationMenuItem>
-                <NavigationMenuTrigger>Components</NavigationMenuTrigger>
-                <NavigationMenuContent>
-                  <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
-                    {components.map((component) => (
-                      <ListItem
-                        key={component.title}
-                        title={component.title}
-                        href={component.href}
-                      >
-                        {component.description}
-                      </ListItem>
-                    ))}
-                  </ul>
-                </NavigationMenuContent>
-              </NavigationMenuItem>
+              <Button variant="outline">FAQ</Button>
             </NavigationMenuList>
           </NavigationMenu>
         </div>
@@ -316,7 +295,7 @@ function App() {
         <div className="pt-24">
           <Separator className="" />
         </div>
-        <footer className="bg-white dark:bg-gray-950">
+        <footer className="bg-white bg-gradient-to-r from-gray-900 dark:bg-gray-950">
           <div className="mx-auto w-full max-w-screen-xl">
             <div className="grid grid-cols-4 gap-8 px-32 py-6 lg:py-8 md:grid-cols-4">
               <div>
@@ -394,47 +373,31 @@ function App() {
                   <Button
                     type="submit"
                     variant="outline"
-                    onClick={() => {
-                      toast({
-                        title: "Scheduled: Catch up ",
-                        description: "Friday, February 10, 2023 at 5:57 PM",
-                        action: (
-                          <ToastAction altText="Goto schedule to undo">
-                            Undo
-                          </ToastAction>
-                        ),
-                      });
-                    }}
+                    onClick={() =>
+                      toast("You're now subscribed to the newsletter!", {
+                        description: new Date().toDateString(),
+                        action: {
+                          label: "Undo",
+                          onClick: () => {
+                            //TODO Remove from newsletter
+                            console.log("Undo");
+                          },
+                        },
+                      })
+                    }
                   >
                     Subscribe
                   </Button>
+                  <Toaster />
                 </div>
               </div>
             </div>
 
-            <span className="text-sm text-gray-500 dark:text-gray-300 sm:text-center">
-              © 2024 <a href="https://flowbite.com/">retrooper</a>
+            <span className="flex justify-between text-sm text-gray-500 dark:text-gray-300 sm:text-center">
+              <ul>© 2024 retrooper</ul>
+              <ul></ul>
             </span>
             <div className="flex mt-4 sm:justify-center md:mt-0 space-x-5 rtl:space-x-reverse">
-              <a
-                href="#"
-                className="text-gray-400 hover:text-gray-900 dark:hover:text-white"
-              >
-                <svg
-                  className="w-4 h-4"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 8 19"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z"
-                    clip-rule="evenodd"
-                  />
-                </svg>
-                <span className="sr-only">Facebook page</span>
-              </a>
               <a
                 href="#"
                 className="text-gray-400 hover:text-gray-900 dark:hover:text-white"
@@ -459,25 +422,6 @@ function App() {
                   aria-hidden="true"
                   xmlns="http://www.w3.org/2000/svg"
                   fill="currentColor"
-                  viewBox="0 0 20 17"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M20 1.892a8.178 8.178 0 0 1-2.355.635 4.074 4.074 0 0 0 1.8-2.235 8.344 8.344 0 0 1-2.605.98A4.13 4.13 0 0 0 13.85 0a4.068 4.068 0 0 0-4.1 4.038 4 4 0 0 0 .105.919A11.705 11.705 0 0 1 1.4.734a4.006 4.006 0 0 0 1.268 5.392 4.165 4.165 0 0 1-1.859-.5v.05A4.057 4.057 0 0 0 4.1 9.635a4.19 4.19 0 0 1-1.856.07 4.108 4.108 0 0 0 3.831 2.807A8.36 8.36 0 0 1 0 14.184 11.732 11.732 0 0 0 6.291 16 11.502 11.502 0 0 0 17.964 4.5c0-.177 0-.35-.012-.523A8.143 8.143 0 0 0 20 1.892Z"
-                    clip-rule="evenodd"
-                  />
-                </svg>
-                <span className="sr-only">Twitter page</span>
-              </a>
-              <a
-                href="#"
-                className="text-gray-400 hover:text-gray-900 dark:hover:text-white"
-              >
-                <svg
-                  className="w-4 h-4"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
                   viewBox="0 0 20 20"
                 >
                   <path
@@ -487,25 +431,6 @@ function App() {
                   />
                 </svg>
                 <span className="sr-only">GitHub account</span>
-              </a>
-              <a
-                href="#"
-                className="text-gray-400 hover:text-gray-900 dark:hover:text-white"
-              >
-                <svg
-                  className="w-4 h-4"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M10 0a10 10 0 1 0 10 10A10.009 10.009 0 0 0 10 0Zm6.613 4.614a8.523 8.523 0 0 1 1.93 5.32 20.094 20.094 0 0 0-5.949-.274c-.059-.149-.122-.292-.184-.441a23.879 23.879 0 0 0-.566-1.239 11.41 11.41 0 0 0 4.769-3.366ZM8 1.707a8.821 8.821 0 0 1 2-.238 8.5 8.5 0 0 1 5.664 2.152 9.608 9.608 0 0 1-4.476 3.087A45.758 45.758 0 0 0 8 1.707ZM1.642 8.262a8.57 8.57 0 0 1 4.73-5.981A53.998 53.998 0 0 1 9.54 7.222a32.078 32.078 0 0 1-7.9 1.04h.002Zm2.01 7.46a8.51 8.51 0 0 1-2.2-5.707v-.262a31.64 31.64 0 0 0 8.777-1.219c.243.477.477.964.692 1.449-.114.032-.227.067-.336.1a13.569 13.569 0 0 0-6.942 5.636l.009.003ZM10 18.556a8.508 8.508 0 0 1-5.243-1.8 11.717 11.717 0 0 1 6.7-5.332.509.509 0 0 1 .055-.02 35.65 35.65 0 0 1 1.819 6.476 8.476 8.476 0 0 1-3.331.676Zm4.772-1.462A37.232 37.232 0 0 0 13.113 11a12.513 12.513 0 0 1 5.321.364 8.56 8.56 0 0 1-3.66 5.73h-.002Z"
-                    clip-rule="evenodd"
-                  />
-                </svg>
-                <span className="sr-only">Dribbble account</span>
               </a>
             </div>
           </div>
