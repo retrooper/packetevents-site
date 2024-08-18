60b453f637f8e954ef282b901c587bc745794cca
retrooper <retrooperdev@gmail.com>
2024-08-18 19:12:25 +0200

    cleaned up the project and split up everything into various files

---
+++ src/PageHeader.tsx
@@ -0,0 +1,39 @@
+import discordLogo from "./assets/discord-mark-white.svg";
+import githubLogo from "./assets/github-mark-white.svg";
+
+import { Button } from "@/components/ui/button";
+import { HoverCard } from "@/components/ui/hover-card";
+
+export default function Header() {
+  return (
+    <>
+      <div className="space-y-5">
+        <div className="absolute top-7 left-0 hover:drop-shadow-[0_35px_35px_rgba(100,120,255,1)]">
+          <div className="flex justify-center mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-200 select-none">
+            packetevents
+          </div>
+        </div>
+        <div className="absolute top-0 right-4 hover:drop-shadow-[0_35px_35px_rgba(100,120,255,0.8)]">
+          <div className="hover:animate-[wiggle_infinite_15s_linear]">
+            <a target="_blank" href="https://github.com/retrooper/packetevents">
+              <img width="35" height="35" src={githubLogo} alt="Logo" />
+            </a>
+          </div>
+        </div>
+        <div className="absolute top-1 right-20 hover:drop-shadow-[0_35px_35px_rgba(100,120,255,0.8)]">
+          <div className="hover:animate-[wiggle_infinite_15s_linear]">
+            <a target="_blank" href="https://discord.gg/DVHxPPxHZc">
+              <img width="40" height="40" src={discordLogo} alt="Logo" />
+            </a>
+          </div>
+        </div>
+        <div className="pl-3 flex justify-center">
+          <HoverCard>
+            <Button variant="ghost">Docs</Button>
+            <Button variant="ghost">Examples</Button>
+          </HoverCard>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ src/PageTitle.tsx
@@ -0,0 +1,15 @@
+export default function PageTitle() {
+  return (
+    <>
+      <div className="flex justify-center mb-4 text-4xl leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+        <h1>Powerful, Versatile. </h1>
+      </div>
+      <div className="flex justify-center mb-4 text-4xl leading-none tracking-tight text-[#444dee] md:text-5xl lg:text-6xl ">
+        <h1>PacketEvents</h1>
+      </div>
+      <div className="flex justify-center mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
+        <h2>Achieve next-level networking by using PacketEvents.</h2>
+      </div>
+    </>
+  );
+}
