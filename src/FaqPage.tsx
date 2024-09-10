4a21b3115a40f57d360c26949fb0c9becfa7911c
Mitchell Gertrude <s3901357@student.rmit.edu.au>
2024-09-10 06:35:13 +0000

    Added FAQ page questions

--- src/FaqPage.tsx
+++ src/FaqPage.tsx
@@ -1,9 +1,17 @@
+import FaqAccordion from "./components/faq/FaqAccordion";
+import Footer from "./components/Footer";
 import NavBar from "./components/NavBar";
 
 const FaqPage = () => {
   return (
     <>
       <NavBar />
+
+      <section>
+        <FaqAccordion />
+      </section>
+
+      <Footer />
     </>
   );
 };
---
+++ src/components/faq/FaqItem.tsx
@@ -0,0 +1,47 @@
+export interface faqProps {
+  index: number;
+  children: any[];
+}
+
+const FaqItem = (props: faqProps) => {
+  return (
+    <>
+      <h2 id={`accordion-collapse-heading-${props.index}`}>
+        <button
+          type="button"
+          class="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-white hover:bg-zinc-700 gap-3"
+          data-accordion-target={`#accordion-collapse-body-${props.index}`}
+          aria-expanded="true"
+          aria-controls={`accordion-collapse-body-${props.index}`}
+        >
+          {props.children[0]}
+          <svg
+            data-accordion-icon
+            class="w-3 h-3 rotate-180 shrink-0"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 10 6"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M9 5 5 1 1 5"
+            />
+          </svg>
+        </button>
+      </h2>
+      <div
+        id={`accordion-collapse-body-${props.index}`}
+        class="hidden"
+        aria-labelledby={`accordion-collapse-heading-${props.index}`}
+      >
+        {props.children[1]}
+      </div>
+    </>
+  );
+};
+
+export default FaqItem;
