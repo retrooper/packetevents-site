e38e9d8a409eae0df425c6ab88de5060251afdee
Mitchell Gertrude <mitchgertrude222@gmail.com>
2024-09-22 09:08:29 +0000

    Added missing xs breakpoint size in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'xs': '360px', // Example breakpoint for extra small devices
+      },
+    },
   },
   plugins: [],
 }
