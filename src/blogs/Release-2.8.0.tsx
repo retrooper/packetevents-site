032fcdb6c04899c12d77daa67376bcf8861aa2c7
retrooper <retrooper@packetevents.com>
2025-05-15 16:19:24 +0200

    Create Release-2.8.0.tsx

---
+++ src/blogs/Release-2.8.0.tsx
@@ -0,0 +1,20 @@
+import { Blog } from "./Blog";
+
+export const Release270: Blog = {
+  date: new Date(2025, 15, 5),
+  title: "PacketEvents 2.8.0 Release",
+  pagelink: "/blog/release-2-8-0",
+  brief: (
+    <div className="text-white">
+      <ul className="list-disc ml-5">
+        <li>Minecraft 1.21.5 support</li>
+      </ul>
+
+      <p className="mt-5">Version 2.8.0 Release is now available</p>
+      <p className="mt-2">
+        This version allows you to test the latest Minecraft 1.21.5 features.
+      </p>
+    </div>
+  ),
+  content: null,
+};
