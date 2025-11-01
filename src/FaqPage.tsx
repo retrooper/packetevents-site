babf175ebf3fd8dc40e64ab57523076024ccab6c
retrooper <retrooperdev@gmail.com>
2025-11-01 18:35:08 +0100

    fix footer overlapping

--- src/FaqPage.tsx
+++ src/FaqPage.tsx
@@ -8,7 +8,7 @@ const FaqPage = () => {
       <NavBar />
 
       <section>
-        <div className=" h-screen mb-40 bg-zinc-900 pt-40">
+        <div className="min-h-screen mb-40 pb-48 bg-zinc-900 pt-40">
           <p className="text-zinc-100 mx-auto text-center text-2xl mb-10 font-medium">
             Frequently Asked Questions
             <div className="w-full max-w-4xl mx-auto mt-5 h-1 bg-indigo-700" />
